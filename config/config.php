a15ace6f72dc596b65241093e71727624aeae378
Ananda Noviesta Wijaya <anandawijayanoviesta@gmail.com>
2026-09-09 22:54:08 +0700

    Add config dan deploy

--- config/config.php
+++ config/config.php
@@ -4,7 +4,7 @@ if (session_status() == PHP_SESSION_NONE) {
 }
 
 // Base URL (Sesuaikan dengan nama folder proyek Anda di htdocs)
-define('BASEURL', 'http://localhost/hotel-reservasi/');
+define('BASEURL', 'http://hotel-reservasi.free.nf');
 
 // Helper untuk proteksi halaman berdasarkan role
 function cek_akses($role_wajib) {
--- config/database.php
+++ config/database.php
@@ -1,8 +1,8 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db   = "hotel_reservasi";
+$host = "sql103.infinityfree.com";
+$user = "if0_42874302";
+$pass = "hewjLQ0VQnM7S9";
+$db   = "if0_42874302_hotel_reservasi";
 
 $conn = mysqli_connect($host, $user, $pass, $db);
 
