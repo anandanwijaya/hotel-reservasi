ced3c42c89fe844c06fea29674465048defc39ad
Ananda Noviesta Wijaya <anandawijayanoviesta@gmail.com>
2026-09-10 16:45:58 +0700

    Fix: Config HTTP ke HTTPS

--- config/config.php
+++ config/config.php
@@ -4,7 +4,7 @@ if (session_status() == PHP_SESSION_NONE) {
 }
 
 // Base URL (Sesuaikan dengan nama folder proyek Anda di htdocs)
-define('BASEURL', 'http://hotel-reservasi.free.nf/');
+define('BASEURL', 'https://hotel-reservasi.free.nf/');
 
 // Helper untuk proteksi halaman berdasarkan role
 function cek_akses($role_wajib) {
