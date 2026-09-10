2861a1fa7adf6407895233a0b7e67168cacc7d9f
Ananda Noviesta Wijaya <anandawijayanoviesta@gmail.com>
2026-09-10 17:54:27 +0700

    Fix: Database Connect

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 require_once 'config/config.php';
-// require_once 'config/database.php';
+require_once 'config/database.php';
 include 'templates/header.php';
 include 'templates/navbar.php';
 ?>
